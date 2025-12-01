b0253994cb10e626ada9f68f509fc41d1b00c7a3
kanchupatipavankumar <pawankumarabd5@gmail.com>
2025-12-01 17:20:09 +0530

    Edit iam-irsa files

--- modules/iam-irsa/main.tf
+++ modules/iam-irsa/main.tf
@@ -1,8 +1,5 @@
-
-
-
 resource "aws_iam_openid_connect_provider" "oidc" {
-  url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  url = var.cluster_oidc_issuer
 
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd30ec5"]
@@ -22,15 +19,9 @@ resource "aws_iam_role" "ebs_csi_role" {
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = {
-          # example: arn issuer without https: "oidc.eks.ap-south-1.amazonaws.com/id/XXXX:sub"
-          "${replace(data.aws_eks_cluster.eks.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${replace(var.cluster_oidc_issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
         }
       }
     }]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "ebs_csi_attach" {
-  role       = aws_iam_role.ebs_csi_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
