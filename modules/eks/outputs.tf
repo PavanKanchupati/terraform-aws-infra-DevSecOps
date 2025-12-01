838b484c6bf9889cfecd8c0723f50de379ccf95a
kanchupatipavankumar <pawankumarabd5@gmail.com>
2025-12-01 17:42:58 +0530

    Edit iam-irsa outputs.tf and eks output.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -9,3 +9,8 @@ output "cluster_endpoint" {
 output "cluster_ca" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
+
+output "cluster_oidc_issuer" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
--- modules/iam-irsa/outputs.tf
+++ modules/iam-irsa/outputs.tf
@@ -2,6 +2,3 @@ output "ebs_csi_role_arn" {
   value = aws_iam_role.ebs_csi_role.arn
 }
 
-output "cluster_oidc_issuer" {
-  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
-}
