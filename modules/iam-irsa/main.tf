bd37fe5299f49d1d31b242cb3d5bd3e48426b975
kanchupatipavankumar <pawankumarabd5@gmail.com>
2025-12-01 17:26:28 +0530

    Edit iam-irsa files again

--- modules/iam-irsa/main.tf
+++ modules/iam-irsa/main.tf
@@ -5,7 +5,6 @@ resource "aws_iam_openid_connect_provider" "oidc" {
   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd30ec5"]
 }
 
-# IRSA role for EBS CSI controller
 resource "aws_iam_role" "ebs_csi_role" {
   name = "${var.cluster_name}-ebs-csi-role"
 
@@ -24,4 +23,9 @@ resource "aws_iam_role" "ebs_csi_role" {
       }
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_attach" {
+  role       = aws_iam_role.ebs_csi_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
