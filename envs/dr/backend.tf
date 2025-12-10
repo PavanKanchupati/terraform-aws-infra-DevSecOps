31aa6124d086fc4eb5c7e6427edcb26905efe978
kanchupatipavankumar <pawankumarabd5@gmail.com>
2025-12-10 18:28:48 +0530

    Keep only dev environment in dev branch (final cleanup)

--- envs/dr/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "kanchupati-pawan-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
